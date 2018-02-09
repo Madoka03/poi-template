d46bac317c019ce306e375ed78c5a95226c4af14
Madoka03 <madoka.kaname@outlook.de>
2018-02-09 15:05:14 +0100

    first version of template

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+
+import App from "./App";
+
+import router from './router.js';
+
+new Vue({
+
+    el: "#app",
+    router,
+    render: h => h(App)
+
+}).$mount('#app');
---
+++ src/routes/ExampleRoute.vue
@@ -0,0 +1,15 @@
+<template>
+
+    <h2>Example Route</h2>
+
+</template>
+
+<script>
+    export default {
+        name: "example-route"
+    }
+</script>
+
+<style scoped>
+
+</style>
